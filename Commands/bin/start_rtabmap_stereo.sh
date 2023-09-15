0437c5b66e1befe8e72c850e86d1ccef62979ed0
WoutStruys <wout.struys@student.pxl.be>
2023-09-15 13:52:59 +0200

    udpate lauch script for stereo rtab map

--- Commands/bin/start_rtabmap_stereo.sh
+++ Commands/bin/start_rtabmap_stereo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 source /opt/ros/noetic/setup.bash
-roslaunch rtabmap_launch rtabmap.launch stereo:=false \
+roslaunch rtabmap_launch rtabmap.launch rtabmap_args:="--Odom/MinInliers 1" \
+                                        stereo:=false \
                                         depth:=true \
+                                        visual_odometry:=false \
+                                        icp_odometry:=false \
                                         rgb_topic:=/camera/rgb/image_raw \
                                         depth_topic:=/camera/depth/image_raw \
                                         camera_info_topic:=/camera/rgb/camera_info \
